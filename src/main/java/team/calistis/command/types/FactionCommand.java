216302201238b1e6dff8aaf07e0078f61ab50a26
KayllonDouglas <kayllon.doug10@gmail.com>
2022-07-02 13:40:40 -0300

    hmmm

--- src/main/java/team/calistis/command/types/FactionCommand.java
+++ src/main/java/team/calistis/command/types/FactionCommand.java
@@ -2,11 +2,11 @@ package team.calistis.command.types;
 
 import cn.nukkit.Player;
 import cn.nukkit.command.CommandSender;
+import lombok.extern.log4j.Log4j2;
 import team.calistis.api.API;
 import team.calistis.command.Command;
-import team.calistis.faction.Faction;
-import team.calistis.faction.FactionMember;
 
+@Log4j2
 public class FactionCommand extends Command {
 
   public FactionCommand() {
@@ -19,13 +19,17 @@ public class FactionCommand extends Command {
   @Override
   public boolean execute(CommandSender sender, String label, String[] args) {
     if (!sender.isPlayer()) return false;
+    Player player = (Player) sender;
+    log.info("Abaixo do 'player'");
     API.getFactionsMap().values().forEach(fc -> {
-      if (!fc.getMembers().containsKey(((Player)sender).getUniqueId())) {
-        sender.sendMessage("isn't in faction");
+      log.info("dentro do loop");
+      if (!fc.getMembers().containsKey(player.getUniqueId())) {
+        player.sendMessage("isn't in faction");
         return;
       }
-      sender.sendMessage("is in faction");
+      player.sendMessage("is in faction");
     });
+    log.info("fora do loop");
     return true;
   }
 
