3979139b1c629746e5bae6f136f227d1f3ae05a6
KayllonDouglas <kayllon.doug10@gmail.com>
2022-07-13 15:52:20 -0300

    Lol, i luv intellij hhmm

--- src/main/java/github/kayllondouglas/CoreListener.java
+++ src/main/java/github/kayllondouglas/CoreListener.java
@@ -3,10 +3,16 @@ package github.kayllondouglas;
 import cn.nukkit.Player;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerAsyncPreLoginEvent;
 import cn.nukkit.event.player.PlayerJoinEvent;
 import cn.nukkit.event.player.PlayerLocallyInitializedEvent;
+import cn.nukkit.event.player.PlayerPreLoginEvent;
 
 public class CoreListener implements Listener {
+
+  public void onLogin(PlayerAsyncPreLoginEvent event) {
+
+  }
 
   @EventHandler
   public void onJoin(PlayerJoinEvent event) {
